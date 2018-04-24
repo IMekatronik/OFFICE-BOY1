8a1fecd6cb4d9319669cf94d511824273765dc60
IMekatronik <36573603+IMekatronik@users.noreply.github.com>
2018-04-24 21:05:20 +0300

    Arduino-Kodu

---
+++ sketch_apr10b.ino
@@ -0,0 +1,32 @@
+int data;
+int led1=7;
+int led2=6;
+void setup() {
+  pinMode(led1,OUTPUT); 
+  pinMode(led2,OUTPUT);
+}
+void loop() { 
+  {
+  if (Serial.available()){
+   int data = Serial.read();
+   delay(100);
+   if(data=='A'){
+     digitalWrite(led1,1);
+     digitalWrite(led2,1);
+   }
+      if(data=='B'){
+     digitalWrite(led1,0);
+     digitalWrite(led2,1);
+     
+     
+   }
+        if(data=='C') {
+          digitalWrite(led1,1);
+          digitalWrite(led2,0);
+         
+        }
+        
+    
+          
+   delay(100);
+  }}}
